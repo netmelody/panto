fa2e746bf2fd4b853310baf1636e983a83e10f41
scarytom <t.denley@cantab.net>
2012-02-28 19:49:16 +0000

    transparency in the striped resource

--- src/main/java/org/netmelody/panto/StripedResourceCollection.java
+++ src/main/java/org/netmelody/panto/StripedResourceCollection.java
@@ -4,21 +4,29 @@ import java.util.Iterator;
 
 import org.apache.tools.ant.types.Resource;
 import org.apache.tools.ant.types.ResourceCollection;
+import org.apache.tools.ant.types.resources.Resources;
 
 public final class StripedResourceCollection implements ResourceCollection {
 
+    private Resources resources = new Resources();
+    
+    public void add(ResourceCollection rc) {
+        resources.add(rc);
+    }
+
+    @SuppressWarnings("unchecked")
     @Override
     public Iterator<Resource> iterator() {
-        return null;
+        return resources.iterator();
     }
 
     @Override
     public int size() {
-        return 0;
+        return resources.size();
     }
 
     @Override
     public boolean isFilesystemOnly() {
-        return false;
+        return resources.isFilesystemOnly();
     }
 }
--- src/test/java/org/netmelody/panto/StripedResourceCollectionTest.java
+++ src/test/java/org/netmelody/panto/StripedResourceCollectionTest.java
@@ -1,6 +1,13 @@
 package org.netmelody.panto;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.tools.ant.types.Resource;
 import org.apache.tools.ant.types.ResourceCollection;
+import org.apache.tools.ant.types.resources.StringResource;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -10,8 +17,30 @@ import static org.hamcrest.Matchers.typeCompatibleWith;
 public final class StripedResourceCollectionTest {
 
     @Test public void
-    implementsResourceCollection() {
+    isAResourceCollection() {
         assertThat(StripedResourceCollection.class, is(typeCompatibleWith(ResourceCollection.class)));
     }
+    
+    @Test public void
+    isTransparentInDefaultConfiguration() throws Exception {
+        final StripedResourceCollection collection = new StripedResourceCollection();
+        
+        collection.add(new StringResource("sausage"));
+        collection.add(new StringResource("turnip"));
+        
+        assertThat(collection.size(), is(2));
+        assertThat(collection.isFilesystemOnly(), is(false));
+        assertThat(contentsOf(collection.iterator()),
+                   Matchers.<Resource>contains(new StringResource("sausage"),
+                                               new StringResource("turnip")));
+    }
 
+    
+    private static Iterable<Resource> contentsOf(Iterator<Resource> iterator) {
+        final List<Resource> result = new ArrayList<Resource>();
+        while (iterator.hasNext()) {
+            result.add(iterator.next());
+        }
+        return result;
+    }
 }
