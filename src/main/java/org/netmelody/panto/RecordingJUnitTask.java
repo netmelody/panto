609a5a387ba303e5c69f337f77ee6f2df963eaef
Tom Denley <tom.denley@youdevise.com>
2012-03-05 09:25:38 +0000

    added more support for nested junit task configuration

--- src/main/java/org/netmelody/panto/RecordingJUnitTask.java
+++ src/main/java/org/netmelody/panto/RecordingJUnitTask.java
@@ -1,13 +1,26 @@
 package org.netmelody.panto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.tools.ant.Project;
+import org.apache.tools.ant.taskdefs.optional.junit.FormatterElement;
 import org.apache.tools.ant.taskdefs.optional.junit.JUnitTask;
+import org.apache.tools.ant.taskdefs.optional.junit.JUnitTask.ForkMode;
+import org.apache.tools.ant.taskdefs.optional.junit.JUnitTask.SummaryAttribute;
 import org.apache.tools.ant.types.resources.Resources;
 
 public final class RecordingJUnitTask {
 
     private final Resources batches = new Resources();
     private final Project project;
+
+    private final List<FormatterElement> formatters = new ArrayList<FormatterElement>();
+
+    private boolean fork;
+    private ForkMode forkMode;
+    private boolean cloneVm;
+    private SummaryAttribute printSummary;
     
     public RecordingJUnitTask(Project project) {
         this.project = project;
@@ -18,10 +31,40 @@ public final class RecordingJUnitTask {
         batches.add(batchTest);
         return batchTest;
     }
-    
+
+    public void setFork(boolean value) {
+        this.fork = value;
+    }
+
+    public void setForkMode(ForkMode mode) {
+        this.forkMode = mode;
+    }
+
+    public void setCloneVm(boolean cloneVm) {
+        this.cloneVm = cloneVm;
+    }
+
+    public void setPrintsummary(SummaryAttribute printSummary) {
+        this.printSummary = printSummary;
+    }
+
+    public void addFormatter(FormatterElement fe) {
+        formatters.add(fe);
+    }
+
     public JUnitTask spawnTaskForStripe(int stripeNumber, int stripeCount) {
         final JUnitTask junit = (JUnitTask) project.createTask("junit");
         junit.createBatchTest().add(new StripedResourceCollection(stripeNumber, stripeCount, batches));
+
+        for (FormatterElement formatter : formatters) {
+            junit.addFormatter(formatter);
+        }
+
+        junit.setFork(fork);
+        junit.setForkMode(forkMode);
+        junit.setCloneVm(cloneVm);
+        junit.setPrintsummary(printSummary);
+
         return junit;
     }
 }
--- src/test/java/org/netmelody/panto/StripedJUnitTaskBuildTest.java
+++ src/test/java/org/netmelody/panto/StripedJUnitTaskBuildTest.java
@@ -6,8 +6,10 @@ import org.junit.Test;
 public final class StripedJUnitTaskBuildTest extends AntBuildTestCase {
 
     @Before
-    public void setup() {
+    public void setup() throws Exception {
         configureProject("StripedJUnitTaskTestBuild.xml");
+        
+        this.folder.newFile("DummyJUnitTestCase.java");
     }
 
     @Test public void
